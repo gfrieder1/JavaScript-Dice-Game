a57c3238e4680e25bb48436d3def4e390e8dfd1c
Gabriel Friederichs <gabriel_friederichs@yaskawa.com>
2022-06-09 14:05:10 -0500

    Randomized Dice Images

--- js/index.js
+++ js/index.js
@@ -1,1 +1,13 @@
-Math.random() * (6 - 1) + 1;
+// generate dice nums
+var diceNum1 = Math.floor(Math.random() * (6 - 1 + 1) + 1); // (max - min + 1) + min = inclusive rand generation
+var diceNum2 = Math.floor(Math.random() * (6 - 1 + 1) + 1);
+
+// create img names
+var diceName1 = "images/dice" + diceNum1 + ".png";
+var diceName2 = "images/dice" + diceNum2 + ".png";
+
+// change images to reflect dice names
+document.querySelector(".img1").setAttribute("src", diceName1);
+document.querySelector(".img2").setAttribute("src", diceName2);
+
+// change title to reflect winner (or draw)
